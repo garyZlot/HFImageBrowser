dfe2578964fa086463d2b60bdaa54fe8a9c43bab
liuguangde <liuguangde@huofar.com>
2015-10-24 16:37:16 +0800

    a simple image browser for UIImageView for ios

---
+++ HFImageBrowserDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HFImageBrowserDemo
+//
+//  Created by liuguangde on 15/10/24.
+//  Copyright © 2015年 liuguangde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
